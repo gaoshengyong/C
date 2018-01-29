aff5ef591a9ebabea03679e09b490a982e1fcd2c
gaoshengyong <gao-shengyong@outlook.com>
2018-01-29 21:47:29 +0800

    subtract.c First Modify

---
+++ subtract.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,sub;
+	printf("Please input two values for subtract:");
+	scanf("%d %d",&a,&b);
+	sub=a-b;
+	printf("%d - %d = %d\n",a,b,sub);
+	return 0;
+}
+
